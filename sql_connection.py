4138cd23a585a82c852ff3863a860e48967e02bf
Mustafa Çakır <86210585+MustafaCakirr@users.noreply.github.com>
2025-01-20 14:35:25 +0300

    Update sql_connection.py

--- sql_connection.py
+++ sql_connection.py
@@ -1,10 +1,10 @@
 import pyodbc as db
 
-# shapefile_path = "C:/Users/MUSTAFA ÇAKIR/OneDrive/Masaüstü/Yeni klasör/ne_110m_admin_0_countries.shp"
+# shapefile_path = "C:/Users/MUSTAFA Ã‡AKIR/OneDrive/MasaÃ¼stÃ¼/Yeni klasÃ¶r/ne_110m_admin_0_countries.shp"
 shapefile_path = "C:/Users/MUSTAFA CAKIR/OneDrive/Masaustu/Yeni klasor/ne_110m_admin_0_countries.shp"
 def get_connection():
     return db.connect('DRIVER={SQL Server};'
-                          'SERVER=DESKTOP-MET5ESU\SQLEXPRESS;'
+                          'SERVER=DESKTOP-******\SQLEXPRESS;' # Enter your custom computer name in the starred area
                           'DATABASE=Refugee;'
                           'Trusted_Connection=True;')
 
